a5c62b7d7a0891f0256f53e6ad55bed0ddb3075b
Quemoy <Quemoy@users.noreply.github.com>
2017-04-22 21:39:14 -0400

    update

--- TSP.m
+++ TSP.m
@@ -3,13 +3,30 @@ cdir = 'C:\Users\johnw\Documents\JHU\JHU_Misc';
 cd(cdir);
 
 %%
-
-
 % distance
 d = [1e6 10 20 5 18; 10 1e6 15 32 10; 20 15 1e6 25 16;
 	5 32 25 1e6 35; 18 10 16 35 1e6];
 cities = ['A', 'B', 'C', 'D', 'E'];
 n = numel(cities);
+
+p = [1, 0]; % parameter of sigmoidal membership function
+T = 100;
+it = 1; % counter for current iteration
+maxIter = 1000; % maximum 1000 iterations
+r = rand(maxIter, 3); % pre-gen rands for speed, need 3 rands per iteration
+
+%% 
+
+% randomized initial route
+tmp = horzcat(rand(5,1), (1:5)'); % generate 5 random number, augment with 1-5
+tmp = sortrows(tmp); % sort the rands, where 2nd col put in order of 1st col
+x = tmp(:,2)'; % the initial randomly generated route;
+
+while it < maxIter
+	
+	
+	it = it + 1;
+end
 
 %% Brute forcing
 
