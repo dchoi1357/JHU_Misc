5aa01a01ff64fd0c84e8e62be0a5e0a5c699d4ef
Quemoy <Quemoy@users.noreply.github.com>
2017-12-05 23:52:03 -0500

    new python code to generate better files

--- Hadoop/project/genExampleFiles.py
+++ Hadoop/project/genExampleFiles.py
@@ -1,50 +1,65 @@
-import random, textwrap, bisect, itertools
+import textwrap, sys, random
+import numpy as np
 
-fruits = ['apple', 'banana', 'orange', 'grape', 'strawberry']
+electronics = ['iPhone', 'LCD', 'HDTV', 'laptop', 'Xbox']
 countries = ['England', 'China', 'Japan', 'France', 'Russia']
-furnitures = ['bed', 'chair', 'lamp', 'table', 'sofa']
-animals = ['dog', 'cat', 'fish', 'bird', 'lion', 'snake']
-clothes = ['shirt', 'pants', 'jacket', 'hat', 'sweater']
-terms = fruits + countries + furnitures + animals + clothes
-with open('SimpleWords400.dat') as f:
-	texts = f.read().splitlines()
+math = ['algebra', 'geometry', 'factorial', 'calculus', 'derivative', 'pi']
+cs = ['sorting', 'optimization', 'queue', 'DFS', 'flops']
+finance = ['options', 'commodities', 'futures', 'ETF', 'bonds']
 
+subjects = ['electronics', 'countries', 'math', 'cs', 'finance']
+terms = [electronics, countries, math, cs, finance]
 punctuations = [' ', ', ', '. ']
 
-def cumsum(l):
-	total = 0
-	cumProbs = list()
-	for p in l:
-		total += p
-		cumProbs.append(total)
-	return cumProbs
+wordCt = [100, 300]
 
-probs = cumsum([0.8, 0.1, 0.1]) # texts, terms, specific terms
-puncts = cumsum([0.8, 0.1, 0.1]) # prob of space vs comma vs period
+words = np.recfromcsv('common_words_freq.csv') # read CSV
+p = np.true_divide(words['frequency'], np.sum(words['frequency']))
 
-wordCt = [300, 500]
+probs1 = np.array([0.9, 0.05, 0.05]) # texts, terms, specific terms
+probs2 = np.array([0.8, 0.1, 0.1]) # prob of space vs comma vs period
 
 def genDoc(spec):
-	corpus = [texts, terms, spec]
-	idx = [bisect.bisect(probs, random.random()) 
-		for x in range(random.randrange(wordCt[0], wordCt[1]))]
-	words = [random.choice(corpus[i]) for i in idx]
-	pt = [punctuations[bisect.bisect(puncts, random.random())] 
-		for x in range(len(idx)-1)]
-	
-	l = list(words[0]) + \
-		list(it.next() for it in itertools.cycle([iter(pt),iter(words)]))
+	ct = np.random.randint(wordCt[0], wordCt[1]) # word count for doc
+	strc = np.random.choice(range(3), ct, True, probs1) # 0=wrd, 1=trm, 2=spec
 
-	t = ''.join(l)
-	return textwrap.wrap(t, 80)
+	txt = np.empty(ct, dtype=words.dtype[0])
+	txt[strc==0] = np.random.choice(words['word'], np.sum(strc==0), True, p)
+	txt[strc==1] = np.random.choice(np.hstack(terms), np.sum(strc==1), True)
+	txt[strc==2] = np.random.choice(spec, np.sum(strc==2), True)
+	
+	puncts = np.random.choice(punctuations, ct, True, probs2)
+	
+	out = np.empty(txt.size*2, dtype=txt.dtype)
+	out[0::2] = txt
+	out[1::2] = puncts
+	out[-1] = '.' # set last punctuation to period.
+
+	return textwrap.wrap(''.join(out), 80)
 
 
 def printListOfTxt(l,name):
-	with open(name, 'w') as f:
+	with open('./docs/'+name, 'w') as f:
 		f.writelines('\n'.join(l))
 
-printListOfTxt(genDoc(fruits), 'sample_fruits.txt')
-printListOfTxt(genDoc(countries), 'sample_countries.txt')
-printListOfTxt(genDoc(furnitures), 'sample_furnitures.txt')
-printListOfTxt(genDoc(animals), 'sample_animals.txt')
-printListOfTxt(genDoc(clothes), 'sample_clothes.txt')
+
+def genRandName(length, n=1):
+	fmt = '%' + '0%dx' % length
+	return [fmt % random.randrange(16**length) for x in range(n)]
+
+
+if len(sys.argv) > 0:
+	nDocs = int(sys.argv[1])
+else:
+	nDocs = 20
+
+fileNames = np.char.add(genRandName(10, nDocs), '.txt')
+subjIdx = np.random.randint(len(subjects), size=nDocs)
+name_x_subj = np.vstack( (fileNames,np.array(subjects)[subjIdx]) ).T
+
+for n,idx in enumerate(subjIdx):
+	printListOfTxt(genDoc(terms[idx]), fileNames[n])
+
+np.savetxt('filename_subject_list.csv', name_x_subj, fmt='%s', delimiter=',',
+	header='fileName,subject', comments='')
+
