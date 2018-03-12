00394cf49b4952063c7c043d444c6771b7c7f042
Quemoy <Quemoy@users.noreply.github.com>
2018-03-12 16:15:37 -0400

    working code, register only

--- GPU/Module06/test_register.cu
+++ GPU/Module06/test_register.cu
@@ -6,41 +6,46 @@
 
 #define DEF_SIMS 20
 #define N_AR 3
-#define START_X 1.000, 0.800, 1.200
-#define PHI 0.227606, 0.689134, -0.315415
+#define START_X 0.800, 0.900, 1.100
+#define PHI -0.315415, 0.427606, 0.189134
+#define C 1
 #define T_MAX 9
 
 unsigned int N_SIMS, N_BYTES;
-unsigned int N_BYTES_PARM = N_AR * size(float);
+unsigned int N_BYTES_PARM = N_AR * sizeof(float);
 
-__constant__ float d_phi[N_AR]; // autoregressive parameters
+__constant__ float c_phi[N_AR]; // autoregressive parameters as constant
 
-void genRandArray(float *arr, const unsigned int n, const float max) {
-	for (int i = 0; i < n; i++) {
-		arr[i] = rand() / (RAND_MAX / max);
+float calcMean(float *arr, unsigned int const n) {
+	float sum = 0.0;
+	for (int i=0; i<n; i++) {
+		sum += arr[i];
 	}
+	return sum / n;
 }
 
-__global__ void calc_w_register(float *x0, float *x1, float *x2) {
+__global__ void calc_w_register(float *x0, float *x1, float *x2, float *x_out) {
 	unsigned int const tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	
-	float r;
+	float w; // white noise for AR process
 	curandState_t state; // initialize rand state
-	currand_init(tid, 0, 0, &state); // set seed to thread indx
+	curand_init(tid, 0, 0, &state); // set seed to thread index
 	
-	// Copy values of X to register
-	float r_x0 = x0[tid]; 
+	float r_x0 = x0[tid]; // Copy values of X to register
 	float r_x1 = x1[tid];
 	float r_x2 = x2[tid];
-	float x;
+	float r_x; // initialize r_x
 	
+	// Simulate AR(n)) process for T_MAX periods
 	for (int t=0; t < T_MAX; t++) {
-		x = d_phi[2] * r_x2 + d_phi[1] * r_x1 + d_phi[0] * r_x0 ;
-		r_x2 = x;
-		r_x3 = r_x2;
+		w = curand_normal(&state) / 2; // w ~ Normal(0, 0.5)
+		r_x = C + c_phi[2] * r_x2 + c_phi[1] * r_x1 + c_phi[0] * r_x0 + w;
 		r_x2 = r_x1;
-		r_x0 = r_x1;
+		r_x1 = r_x0;
+		r_x0 = r_x;
 	}
+	
+	x_out[tid] = r_x; // save x as output
 }
 
 int main(int argc, char* argv[]) {
@@ -50,31 +55,48 @@ int main(int argc, char* argv[]) {
 		N_SIMS = DEF_SIMS;
 	}
 	N_BYTES = N_SIMS * sizeof(float); // size of array 
+	printf("Running %u simulations...\n", N_SIMS);
 	
+	float *h_x0, *h_x1, *h_x2, *h_x; 
+	h_x0 = (float*) malloc(N_BYTES); // allocate input
 	h_x1 = (float*) malloc(N_BYTES); // allocate input
 	h_x2 = (float*) malloc(N_BYTES); // allocate input
-	h_x3 = (float*) malloc(N_BYTES); // allocate input
-	
-	float *h_x0, *h_x1, *h_x2;
-	float start_x = {START_X};
-	for (int i = 0; i < N_SIMS; i++) { // set all X0 to the same number
-		// x0s[i] = rand() / (RAND_MAX / 1.0);
+	h_x = (float*) malloc(N_BYTES); // allocate input
+		
+	float start_x [N_AR] = {START_X};
+	for (int i = 0; i < N_SIMS; i++) { // set all host Xs to the same number
 		h_x0[i] = start_x[0];
 		h_x1[i] = start_x[1];
 		h_x2[i] = start_x[2];
 	}
 	
-	float *d_x0, *d_x1, *d_x2; // device memory for storing X
-	cudaMalloc((void **)&d_X, N_BYTES); // device input
-	cudaMalloc((void **)&d_X, N_BYTES); // device input
-	cudaMalloc((void **)&d_X, N_BYTES); // device input
+	float *d_x0, *d_x1, *d_x2, *d_out; // device memory for storing X
+	cudaMalloc((void **)&d_x0, N_BYTES); // device input
+	cudaMalloc((void **)&d_x1, N_BYTES); // device input
+	cudaMalloc((void **)&d_x2, N_BYTES); // device input
+	cudaMalloc((void **)&d_out, N_BYTES); // device input
 	cudaMemcpy(d_x0, h_x0, N_BYTES, cudaMemcpyHostToDevice); //copy to device
 	cudaMemcpy(d_x1, h_x1, N_BYTES, cudaMemcpyHostToDevice); //copy to device
 	cudaMemcpy(d_x2, h_x2, N_BYTES, cudaMemcpyHostToDevice); //copy to device
 	
-	float h_phi [] = {PHI};
-	d_phi = (float*) malloc(N_BYTES_PARM); // initialize AR parameters
-	cudaMemcpyToSymbol(d_phi, h_phi, N_BYTES_PARM); // copy to constant
+	float h_phi [N_AR] = {PHI};
+	cudaMemcpyToSymbol(c_phi, h_phi, N_BYTES_PARM); // copy params to constant
 	
-	calc_w_register<<<1, N_SIMS>>>(d_x1, d_x2, d_x3);
+	calc_w_register<<<1, N_SIMS>>>(d_x0, d_x1, d_x2, d_out); // kernel
+	cudaMemcpy(h_x, d_out, N_BYTES, cudaMemcpyDeviceToHost ); // copy back res
+	cudaMemcpy(h_x0, d_x0, N_BYTES, cudaMemcpyDeviceToHost );
+	cudaMemcpy(h_x1, d_x1, N_BYTES, cudaMemcpyDeviceToHost );
+	cudaMemcpy(h_x2, d_x2, N_BYTES, cudaMemcpyDeviceToHost );
+	
+	for (int i=0; i<N_SIMS; i++) {
+		printf("%.4f ", h_x[i]);
+		if (i%10 == 9) {
+			printf("\n");
+		}
+	}
+	float mean = calcMean(h_x, N_SIMS);
+	printf("\nResult of simulation is: %f\n", mean);
+	
+	cudaFree(d_x2); cudaFree(d_x1); cudaFree(d_x0); cudaFree(d_out);
+	free(h_x0); free(h_x1); free(h_x2); free(h_x);
 }
