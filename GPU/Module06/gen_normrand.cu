e6714056ee24647dcbf5c864a6b77b8e0cf02bb5
Quemoy <Quemoy@users.noreply.github.com>
2018-03-12 12:17:27 -0400

    non-working prototype

--- GPU/Module06/gen_normrand.cu
+++ GPU/Module06/gen_normrand.cu
@@ -6,7 +6,7 @@
 #define MAX 100
 
 /* this GPU kernel function calculates a random number and stores it in the parameter */
-__global__ void random(float* result) {
+__global__ void random(float* result1, float* result2) {
     /* CUDA's random number library uses curandState_t to keep track of the seed value
        we will store a random state for every thread  */
     curandState_t state;
@@ -18,25 +18,30 @@ __global__ void random(float* result) {
             &state);
 
     /* curand works like rand - except that it takes a state as a parameter */
-    *result = curand_normal(&state);
+    *result1 = curand_normal(&state);
+	*result2 = curand_normal(&state);
 }
 
 int main( ) {
     /* allocate an int on the GPU */
-    float* gpu_x;
-    cudaMalloc((void**) &gpu_x, sizeof(float));
+    float* gpu_x1, * gpu_x2;
+    cudaMalloc((void**) &gpu_x1, sizeof(float));
+	cudaMalloc((void**) &gpu_x2, sizeof(float));
 
     /* invoke the GPU to initialize all of the random states */
-    random<<<1, 1>>>(gpu_x);
+    random<<<1, 1>>>(gpu_x1, gpu_x2);
 
     /* copy the random number back */
-    float x;
-    cudaMemcpy(&x, gpu_x, sizeof(float), cudaMemcpyDeviceToHost);
+    float x1, x2;
+    cudaMemcpy(&x1, gpu_x1, sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(&x2, gpu_x2, sizeof(float), cudaMemcpyDeviceToHost);
 
-    printf("Random number = %f.\n", x);
+    printf("Random number = %f.\n", x1);
+	printf("Random number = %f.\n", x2);
 
     /* free the memory we allocated */
-    cudaFree(gpu_x);
+    cudaFree(gpu_x1);
+	cudaFree(gpu_x2);
 
     return 0;
 }
--- GPU/Module06/test_register.cu
+++ GPU/Module06/test_register.cu
@@ -5,9 +5,43 @@
 #include <curand_kernel.h>
 
 #define DEF_SIMS 20
-#define X0 1.0342
+#define N_AR 3
+#define START_X 1.000, 0.800, 1.200
+#define PHI 0.227606, 0.689134, -0.315415
+#define T_MAX 9
 
 unsigned int N_SIMS, N_BYTES;
+unsigned int N_BYTES_PARM = N_AR * size(float);
+
+__constant__ float d_phi[N_AR]; // autoregressive parameters
+
+void genRandArray(float *arr, const unsigned int n, const float max) {
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() / (RAND_MAX / max);
+	}
+}
+
+__global__ void calc_w_register(float *x0, float *x1, float *x2) {
+	unsigned int const tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+	
+	float r;
+	curandState_t state; // initialize rand state
+	currand_init(tid, 0, 0, &state); // set seed to thread indx
+	
+	// Copy values of X to register
+	float r_x0 = x0[tid]; 
+	float r_x1 = x1[tid];
+	float r_x2 = x2[tid];
+	float x;
+	
+	for (int t=0; t < T_MAX; t++) {
+		x = d_phi[2] * r_x2 + d_phi[1] * r_x1 + d_phi[0] * r_x0 ;
+		r_x2 = x;
+		r_x3 = r_x2;
+		r_x2 = r_x1;
+		r_x0 = r_x1;
+	}
+}
 
 int main(int argc, char* argv[]) {
 	if (argc > 1) { // no command line inputs
@@ -15,9 +49,32 @@ int main(int argc, char* argv[]) {
 	} else {
 		N_SIMS = DEF_SIMS;
 	}
+	N_BYTES = N_SIMS * sizeof(float); // size of array 
 	
-	N_BYTES = N_SIMS * sizeof(float);
-	float *d_X;
+	h_x1 = (float*) malloc(N_BYTES); // allocate input
+	h_x2 = (float*) malloc(N_BYTES); // allocate input
+	h_x3 = (float*) malloc(N_BYTES); // allocate input
+	
+	float *h_x0, *h_x1, *h_x2;
+	float start_x = {START_X};
+	for (int i = 0; i < N_SIMS; i++) { // set all X0 to the same number
+		// x0s[i] = rand() / (RAND_MAX / 1.0);
+		h_x0[i] = start_x[0];
+		h_x1[i] = start_x[1];
+		h_x2[i] = start_x[2];
+	}
+	
+	float *d_x0, *d_x1, *d_x2; // device memory for storing X
 	cudaMalloc((void **)&d_X, N_BYTES); // device input
+	cudaMalloc((void **)&d_X, N_BYTES); // device input
+	cudaMalloc((void **)&d_X, N_BYTES); // device input
+	cudaMemcpy(d_x0, h_x0, N_BYTES, cudaMemcpyHostToDevice); //copy to device
+	cudaMemcpy(d_x1, h_x1, N_BYTES, cudaMemcpyHostToDevice); //copy to device
+	cudaMemcpy(d_x2, h_x2, N_BYTES, cudaMemcpyHostToDevice); //copy to device
 	
+	float h_phi [] = {PHI};
+	d_phi = (float*) malloc(N_BYTES_PARM); // initialize AR parameters
+	cudaMemcpyToSymbol(d_phi, h_phi, N_BYTES_PARM); // copy to constant
+	
+	calc_w_register<<<1, N_SIMS>>>(d_x1, d_x2, d_x3);
 }
