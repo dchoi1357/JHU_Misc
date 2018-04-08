e8b9ba8a64a8d54182cd03b0936df95e056550f6
Quemoy <Quemoy@users.noreply.github.com>
2018-04-08 04:03:08 -0400

    working NPP

--- GPU/Module09/npp_examples/nppExamples.cpp
+++ GPU/Module09/npp_examples/nppExamples.cpp
@@ -19,74 +19,99 @@
 
 #include <helper_string.h>
 #include <helper_cuda.h>
+
+void mirrorFunc(const char* filePath, NppiAxis mirrorType, std::string suff) {
+	std::string sFilename = filePath;
+
+	// if we specify the filename at the command line, then we only test sFilename[0].
+	int file_errors = 0;
+	std::ifstream infile(sFilename.data(), std::ifstream::in);
+
+	if (infile.good()) {
+		printf("\topened: <%s> successfully!\n", sFilename.data());
+		infile.close();
+	} else {
+		printf("\tunable to open: <%s> \n", sFilename.data());
+		file_errors++;
+		infile.close();
+		exit(EXIT_FAILURE);
+	}
+
+	std::string sResultFilename = sFilename;
+	std::string::size_type dot = sResultFilename.rfind('.');
+
+	if (dot != std::string::npos) {
+		sResultFilename = sResultFilename.substr(0, dot);
+	}
+	sResultFilename += ("_mirrored_" + suff + ".pgm");
+
+	// declare a host image object for an 8-bit grayscale image
+	npp::ImageCPU_8u_C1 oHostSrc;
+	// load gray-scale image from disk
+	npp::loadImage(sFilename, oHostSrc);
+	// declare a device image and copy construct from the host image,
+	// i.e. upload host to device
+	npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
+
+	// create struct with ROI size
+	NppiSize oSizeROI = {(int)oDeviceSrc.width() , (int)oDeviceSrc.height() };
+	
+	// allocate device image of appropriately reduced size
+	npp::ImageNPP_8u_C1 oDeviceDst(oSizeROI.width, oSizeROI.height);
+	
+	NPP_CHECK_NPP ( // run mirroring
+		nppiMirror_8u_C1R(	oDeviceSrc.data(), oDeviceSrc.pitch(),
+							oDeviceDst.data(), oDeviceDst.pitch(),
+							oSizeROI, mirrorType) );
+
+	// declare a host image for the result
+	npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
+	// and copy the device result data into it
+	oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
+
+	saveImage(sResultFilename, oHostDst);
+	std::cout << "\tSaved image: " << sResultFilename << std::endl;
+
+	nppiFree(oDeviceSrc.data());
+	nppiFree(oDeviceDst.data());
+}
+
+
 int main(int argc, char *argv[]) {
-	printf("%s Starting...\n\n", argv[0]);
-
+	char *filePath;
+	int flipType;
+	if (argc == 2) { // if an input file was specified
+		filePath = sdkFindFilePath(argv[1], argv[0]);
+		flipType = 1;
+	} else if (argc > 2) {
+		filePath = sdkFindFilePath(argv[1], argv[0]);
+		flipType = std::max( std::min(atoi(argv[2]), 3), 1);
+	} else {
+		filePath = sdkFindFilePath("Lena.pgm", argv[0]); 
+		flipType = 1;
+	}
+	
+	const char *flipName;
+	std::string fSuffix;
+	NppiAxis ax;
+	if (flipType == 1) {
+		flipName = "horizontally";
+		fSuffix = "horiz";
+		ax = NPP_HORIZONTAL_AXIS;
+	} else if (flipType == 2) {
+		flipName = "vertically";
+		fSuffix = "vert";
+		ax = NPP_VERTICAL_AXIS;
+	} else {
+		flipName = "horizontally and vertically";
+		fSuffix = "horiz_vert"; 
+		ax = NPP_BOTH_AXIS;
+	}
+	
+	
 	try {
-		std::string sFilename;
-		char *filePath;
-		filePath = sdkFindFilePath("Lena.pgm", argv[0]);
-
-		if (filePath) {
-			sFilename = filePath;
-		} else {
-			sFilename = "Lena.pgm";
-		}
-
-		// if we specify the filename at the command line, then we only test sFilename[0].
-		int file_errors = 0;
-		std::ifstream infile(sFilename.data(), std::ifstream::in);
-
-		if (infile.good()) {
-			printf("%s opened: <%s> successfully!\n", argv[0], sFilename.data());
-			infile.close();
-		} else {
-			printf("%s  unable to open: <%s> \n", argv[0], sFilename.data());
-			file_errors++;
-			infile.close();
-			exit(EXIT_FAILURE);
-		}
-
-		std::string sResultFilename = sFilename;
-		std::string::size_type dot = sResultFilename.rfind('.');
-
-		if (dot != std::string::npos) {
-			sResultFilename = sResultFilename.substr(0, dot);
-		}
-		sResultFilename += "_mirrored.pgm";
-
-		// declare a host image object for an 8-bit grayscale image
-		npp::ImageCPU_8u_C1 oHostSrc;
-		// load gray-scale image from disk
-		npp::loadImage(sFilename, oHostSrc);
-		// declare a device image and copy construct from the host image,
-		// i.e. upload host to device
-		npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
-
-		// create struct with ROI size
-		NppiSize oSizeROI = {(int)oDeviceSrc.width() , (int)oDeviceSrc.height() };
-		
-		// allocate device image of appropriately reduced size
-		npp::ImageNPP_8u_C1 oDeviceDst(oSizeROI.width, oSizeROI.height);
-		
-		NPP_CHECK_NPP ( // run mirroring
-			nppiMirror_8u_C1R(	oDeviceSrc.data(), oDeviceSrc.pitch(),
-								oDeviceDst.data(), oDeviceDst.pitch(),
-								oSizeROI, NPP_VERTICAL_AXIS) );
-
-		// declare a host image for the result
-		npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
-		// and copy the device result data into it
-		oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
-
-		saveImage(sResultFilename, oHostDst);
-		std::cout << "Saved image: " << sResultFilename << std::endl;
-
-		nppiFree(oDeviceSrc.data());
-		nppiFree(oDeviceDst.data());
-
-		exit(EXIT_SUCCESS);
-
+		printf("Mirroring %s %s...\n",  filePath, flipName);
+		mirrorFunc(filePath, ax, fSuffix);
 	} catch (npp::Exception &rException){
 		std::cerr << "Program error! The following exception occurred: \n";
 		std::cerr << rException << std::endl;
@@ -100,6 +125,6 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 		return -1;
 	}
-
+	exit(EXIT_SUCCESS);
 	return 0;
 }
---
+++ GPU/Module09/run.sh
@@ -0,0 +1,6 @@
+cd ./npp_examples
+make
+./nppExamples oswald.pgm 1
+./nppExamples oswald.pgm 2
+./nppExamples oswald.pgm 3
+rm nppExamples nppExamples.o
