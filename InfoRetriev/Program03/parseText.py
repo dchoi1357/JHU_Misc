5016fb0bc2aaeed71a91c1cdf109994d425dc587
Quemoy <Quemoy@users.noreply.github.com>
2018-10-03 20:36:05 -0500

    First draft Program 03

---
+++ InfoRetriev/Program03/parseText.py
@@ -0,0 +1,83 @@
+import sys, nltk, re, string, struct, pickle
+
+tg = re.compile(r'<p id=(\d+)>', re.IGNORECASE) # regex to capture docID
+
+pct = re.compile('^['+string.punctuation+']+$') # match 1+ consec. punctuation
+vcb = dict() # dictionary for entire collection
+
+def processDocsFile(fname):
+	inDoc = False # whether currently in the middle of parsing a Doc
+	docID = 0 # the current document ID the program is in
+
+	with open (fname, 'r') as f:
+		for line in f: # NOTE: must read file line by line due to large size
+			if not inDoc: # before start of a document
+				docID = int( tg.findall(line)[0] ) # assume 1st match is right
+				inDoc = True
+				tmpTxt = '' # accumulate text for processing by document
+			else: # in the middle of parsing a document
+				if line.strip().casefold() != '</p>': # not end of document
+					tmpTxt += line.casefold()
+				else:
+					inDoc = False # set to no longer in document
+					tmpDict = processDoc(tmpTxt)
+
+def processDoc(txt, docid):
+	d = dict() # temp dict to store info on this document
+	for tk in nltk.word_tokenize(rawTxt): # split into tokens
+		if not pct.match(tk): # if token is not (1+ consec.) punctuations
+			d[tk] = d.get(tk, 0) + 1
+	for tk in d: # merge dict of this doc with the bigger vocab dict
+		if tk not in vcb: # if not in vocab
+			vcb[tk] = [(docid, d[tk])] # add first posting 
+		else: # if already in vocab
+			vcb[tk].append( (docid, d[tk]) ) # append to posting list
+	return d
+		
+
+
+def processDoc(rawTxt, docid):
+	d = dict() # temp dict to store info on this document
+	for tk in nltk.word_tokenize(rawTxt): # split into tokens
+		if not pct.match(tk): # if token is not (1+ consec.) punctuations
+			d[tk] = d.get(tk, 0) + 1
+	for tk in d: # merge dict of this doc with the bigger vocab dict
+		if tk not in vcb: # if not in vocab
+			vcb[tk] = [(docid, d[tk])] # add first posting 
+		else: # if already in vocab
+			vcb[tk].append( (docid, d[tk]) ) # append to posting list
+	return d
+
+def writeInvertedFile(invFileName, dictFileName):
+	outDict = dict() # dictionary to be written out
+	count, coll = 0,0 # running offset count and collection size
+	with open(invFileName, 'wb') as f:
+		for term in vcb:
+			outDict[term] = (len(vcb[term]), count)
+			count += 2*len(vcb[term]) # increment current count
+			for docid,n in vcb[term]: # write tuple of docid and tf(term,docid)
+				coll += n
+				f.write( docid.to_bytes(4, byteorder='little') )
+				f.write( n.to_bytes(4, byteorder='little') )
+
+	with open(dictFileName, 'wb') as h: # write dic out as pickle file
+		pickle.dump(outDict, h, protocol=pickle.HIGHEST_PROTOCOL)
+	return outDict, coll
+
+def processFile(fname):
+	print('Processing file: %s'%fname)
+	paragraphs = getParagraphs(fname)
+	for n,doc in enumerate(paragraphs): # loop over all document
+		processDoc(doc, n+1) # parse each doc and update full dictionary
+	_,coll = writeInvertedFile(sys.argv[2], sys.argv[3]) # write inv file and dic
+	print('\tProcessed %d paragraphs'%len(paragraphs))
+	print('\tVocabulary size: %d'%len(vcb))
+	print('\tCollection size: %d'%coll)
+
+	print('Wrote inverted file %s, dict file %s.' % (sys.argv[2],sys.argv[3]) )
+
+
+if len(sys.argv) == 4:
+	processFile(sys.argv[1])
+else:
+	print("Usage: %s [inputFile] [outInvFileName] [outDictName]"%sys.argv[0])
