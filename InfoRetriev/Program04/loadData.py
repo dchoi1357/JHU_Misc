0610ceccdd620692f7f9f6207423277124c5a363
Quemoy <Quemoy@users.noreply.github.com>
2018-10-29 08:00:02 -0400

    pre-submit

--- InfoRetriev/Program04/loadData.py
+++ InfoRetriev/Program04/loadData.py
@@ -1,21 +1,31 @@
-import sys, nltk, os
+import sys, nltk, os, sklearn
 import tokenHelper as tkn # import custom function for tokenization of text
 import numpy as np
 
-#d = 'E:\\VirtualMachines\\shared\\tmp\\'
-#trainFile = 'E:\\VirtualMachines\\shared\\tmp\\phase1.train.shuf.tsv'
-#test = 'E:\\VirtualMachines\\shared\\tmp\\test.tsv'
-
-d = os.path.join('c:', 'data', 'JHU', 'InfoRetriev', 'Program04')
+d = os.path.join('e:', 'VirtualMachines', 'shared', 'tmp')
+# d = os.path.join('c:', 'data', 'JHU', 'InfoRetriev', 'Program04')
 trainFile = os.path.join(d, 'phase1.train.shuf.tsv')
+devFile = os.path.join(d, 'phase1.dev.shuf.tsv')
 testFile = os.path.join(d, 'phase1.test.shuf.tsv')
 
-#trainFile = './data/phase1.train.shuf.tsv'
-
 header = ['assessment','docID','title','authors','journal','ISSN','year',
-		'language', 'abstract', 'keywords']
+		'language', 'abstract', 'keywords'] # the variable names for file
 
-convFun = {0: lambda s: int(s)>0, 1: lambda b: b[5:]}
-raw = np.genfromtxt(trainFile, delimiter='\t', dtype=None, names=header,
-					comments=None, converters=convFun, encoding='utf-8')
+def readFile(fName):
+	convFun = {1: lambda b: b[5:]} # to capture hash code without prefix
+	return np.genfromtxt(fName, delimiter='\t', dtype=None, names=header,
+		comments=None, converters=convFun, encoding='utf-8') # load file
 
+raw = readFile(trainFile)
+binary = sklearn.preprocessing.LabelBinarizer(pos_label=1) # 1 = positive class
+labels = binary.fit_transform(raw['assessment']).ravel()  # binarize assessments
+
+# The following code tokenizes input text using TF-IDF and get features
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorize = TfidfVectorizer(tokenizer=tkn.tokenizeNoPunctStopword)
+x = vectorize.fit_transform( raw['title'] ) # extract features
+
+from sklearn import naive_bayes as NB 
+mn_nb = NB.MultinomialNB().fit(x, labels)
+bern_nb = NB.BernoulliNB().fit(x, labels)
+#comp_nb = NB.Compl
